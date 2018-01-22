54f17171bd613cab8f99b6d65ac8eca9208246f6
Eduard <a>
2018-01-22 00:04:22 -0800

    Migrate to grid layout

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -3,14 +3,10 @@ import uirouter from '@uirouter/angularjs';
 
 import routing from './home.routes';
 import HomeController from './controller/src/home.controller';
-import begin from '../home/directive/src/begin-section';
-import footer from '../commons/directive/src/footer';
 import header from '../commons/directive/src/header';
-import us from '../home/directive/src/us-section';
-import contact from '../home/directive/src/contact-section';
-import services from "./directive/src/services-section"
 
-export default angular.module('app.home', [uirouter, header, footer, begin, us, contact,services])
+
+export default angular.module('app.home', [uirouter, header])
     .config(routing)
     .controller('HomeController', HomeController)
     .name;
