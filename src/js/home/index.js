78ddc0675f861ec4a0e1a4b28578c48e3cbda3cf
Eduard <a>
2018-01-23 23:25:59 -0800

    Begin contact and header declared sections

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -4,9 +4,10 @@ import uirouter from '@uirouter/angularjs';
 import routing from './home.routes';
 import HomeController from './controller/src/home.controller';
 import header from '../commons/directive/src/header';
+import begin from '../home/directive/src/begin-section'
+import contact from "../home/directive/src/contact-section"
 
-
-export default angular.module('app.home', [uirouter, header])
+export default angular.module('app.home', [uirouter, header, begin, contact])
     .config(routing)
     .controller('HomeController', HomeController)
     .name;
