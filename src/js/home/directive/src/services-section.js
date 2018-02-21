f615d2d405d3a014b8558ada1741cd83b07c510a
EdgarAndresSanchezHernandez <eidar_62@hotmail.com>
2018-02-21 15:41:21 -0800

    Last versión for TechnoMind Web Page

--- src/js/home/directive/src/services-section.js
+++ src/js/home/directive/src/services-section.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import template from '../template/services-section.html';
+import template from '../template/technologies-section.html';
 
 /**
  * Directive for inject  service section
