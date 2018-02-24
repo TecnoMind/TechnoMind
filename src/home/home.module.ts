b567d2c29b837f054a7766dfc09008f2eec64bf5
Eduard <a>
2018-02-23 22:02:39 -0800

    Cleaning  package

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -5,12 +5,15 @@ import {NgbModule, NgbTypeaheadModule} from "@ng-bootstrap/ng-bootstrap";
 import {routing} from "./home.routes";
 import {HomeComponent} from "./component/src/home.component";
 import {BeginComponent} from "./component/src/begin.component";
+import { ParticlesModule } from 'angular-particle';
+
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         NgbModule.forRoot(),
+        ParticlesModule,
         NgbTypeaheadModule.forRoot(),
         routing
     ],
