59e118f938e29136f48b84661dd657b9de1fd08f
Eduardo-Escalona <eduardo.escalona@lkmx.io>
2018-02-23 16:39:55 -0800

    Migrate angular 2 webpack scss

--- src/home/component/src/home.component.ts
+++ src/home/component/src/home.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import "../../../../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../../../css/main.scss"
 @Component({
     selector: 'my-app',
     template: require('../template/home.component.html'),
