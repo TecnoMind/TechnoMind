106136bddf64c7ec869557b920bee5d2071e54bf
Eduard <a>
2018-03-03 14:13:35 -0800

    Add loading view

--- src/home/component/src/us.component.ts
+++ src/home/component/src/us.component.ts
@@ -1,8 +1,15 @@
 import {Component} from "@angular/core";
+import {trigger, transition, animate, state, style,keyframes} from "@angular/animations";
 
 @Component({
     selector: 'us',
     template: require('../template/us.component.html'),
+    animations: [
+
+        trigger('labelIn', [
+            transition('inactive => active', animate('1000ms ease-in')),
+        ]),
+    ]
 })
 
 export class UsComponent {
