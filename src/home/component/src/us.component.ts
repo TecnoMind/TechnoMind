6e9eaf59496767d8c5012887370e1221582c3c09
EdgarAndresSanchezHernandez <eidar_62@hotmail.com>
2018-03-19 11:31:34 -0700

    Last versión of TechnoMind Web Page

--- src/home/component/src/us.component.ts
+++ src/home/component/src/us.component.ts
@@ -6,5 +6,41 @@ import {Component} from "@angular/core";
 })
 
 export class UsComponent {
+    showSelected: boolean;
+    showSelected2: boolean;
+    showSelected3: boolean;
+    showSelected4: boolean;
+
+    constructor() {
+        this.showSelected = true;
+        this.showSelected2 = false;
+        this.showSelected3 = false;
+        this.showSelected4 = false;
+    }
+
+    softwareButton() {
+        this.showSelected = true;
+        this.showSelected2 = false;
+        this.showSelected3 = false;
+        this.showSelected4 = false;
+    }
+    consultoryButton() {
+        this.showSelected = false;
+        this.showSelected2 = true;
+        this.showSelected3 = false;
+        this.showSelected4 = false;
+    }
+    mobileButton() {
+        this.showSelected = false;
+        this.showSelected2 = false;
+        this.showSelected3 = true;
+        this.showSelected4 = false;
+    }
+    designButton() {
+        this.showSelected = false;
+        this.showSelected2 = false;
+        this.showSelected3 = false;
+        this.showSelected4 = true;
+    }
 
 }
