18cf0fc2e492c9ba39cb2ca2c177467c82419fcf
Eduard <a>
2018-03-22 22:27:26 -0700

    Beginning Routing animation

--- src/home/component/src/app.component.ts
+++ src/home/component/src/app.component.ts
@@ -1,9 +1,8 @@
 import {Component} from "@angular/core";
 
+
 @Component({
     selector: 'my-app',
-    template: require('../template/app.component.html'),
+    template: require('../template/app.component.html')
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
